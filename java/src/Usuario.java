855b9366009d5a631a8d36c879e6db118a6b1413
AgustinABR <amuriels001@iesalbarregas.es>
2026-03-13 19:52:38 +0100

    cambios usuario

--- java/src/Usuario.java
+++ java/src/Usuario.java
@@ -1,3 +1,36 @@
 public class Usuario {
     private AlgoritmoPresentacion algoritmoSeleccionado;
+    private String nombre;
+    private String contrasenia;
+
+
+    public Usuario(String nombre, String contrasenia, AlgoritmoPresentacion algoritmoSeleccionado) {
+        this.algoritmoPresentacion = setAlgoritmoSeleccionado(algoritmoSeleccionado);
+    }
+
+    public AlgoritmoPresentacion getAlgoritmoSeleccionado() {
+        return algoritmoSeleccionado;
+    }
+
+    public void setAlgoritmoSeleccionado(AlgoritmoPresentacion algoritmoSeleccionado) {
+        this.algoritmoSeleccionado = algoritmoSeleccionado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+
 }
