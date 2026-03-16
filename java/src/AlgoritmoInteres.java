a6d98dcdf8066cf8a1ccebf977a3248254f20941
AgustinABR <amuriels001@iesalbarregas.es>
2026-03-16 19:40:51 +0100

    agregar el metodo sin completar en los algoritmos

--- java/src/AlgoritmoInteres.java
+++ java/src/AlgoritmoInteres.java
@@ -1,3 +1,12 @@
+
+import java.util.List;
+
 public class AlgoritmoInteres implements AlgoritmoPresentacion {
+
+    @Override
+    public List<Publicacion> obtenerContenido(Usuario usuario, List<Publicacion> publicaciones) {
+        //hacer
+        return null;
+    }
     
 }
--- java/src/AlgoritmoSeguido.java
+++ java/src/AlgoritmoSeguido.java
@@ -1,3 +1,9 @@
+import java.util.List;
+
 public class AlgoritmoSeguido implements AlgoritmoPresentacion {
-    
+    @Override
+    public List<Publicacion> obtenerContenido(Usuario usuario, List<Publicacion> publicaciones) {
+        //hacer
+        return null;
+    }
 }
