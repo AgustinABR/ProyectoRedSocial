1bb72749bd70e8b9082568fd16502db63f28ae69
AgustinABR <amuriels001@iesalbarregas.es>
2026-03-21 18:10:16 +0100

    equals y hascode en usuario

--- java/src/Usuario.java
+++ java/src/Usuario.java
@@ -6,6 +6,7 @@ public class Usuario {
     private String contrasena;
     private ArrayList<Usuario> seguidos;
     private ArrayList<Etiqueta> intereses;
+    private AlgoritmoPresentacion algoritmo;
 
     public Usuario(String nombre, String contrasena) {
         this.nombre = nombre;
@@ -39,5 +40,26 @@ public class Usuario {
     public void agregarInteres(Etiqueta interes) {
         intereses.clear();
         intereses.add(interes);
+    }
+
+    public AlgoritmoPresentacion getAlgoritmo() {
+        return algoritmo;
+    }
+
+    public void setAlgoritmo(AlgoritmoPresentacion algoritmo) {
+        this.algoritmo = algoritmo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario)) return false;
+        Usuario u = (Usuario) o;
+        return nombre.equals(u.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return nombre.hashCode();
     }
 }
